9560c312b640cdd0b8aa4606f3d5f0b4e39aaaf6
PauloKrane <paulo211082@gmail.com>
2024-02-06 21:20:24 -0300

    portfolio 0.2

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,33 @@ desplazarArriba.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+
+//Para lograr que una sección aparezca centrada en la pantalla al presionar un enlace en el menú
+document.addEventListener("DOMContentLoaded", function () {
+  var links = document.querySelectorAll('.nav-link');
+  
+  links.forEach(function (link) {
+      link.addEventListener('click', function (e) {
+          e.preventDefault();
+
+          var targetId = this.getAttribute('href').substring(1);
+          var targetElement = document.getElementById(targetId);
+
+          if (targetElement) {
+              var offsetTop = targetElement.offsetTop;
+              var body = document.body;
+              var html = document.documentElement;
+              var scrollTop = body.scrollTop || html.scrollTop;
+
+              // Centra la sección en la pantalla
+              var scrollTarget = offsetTop - (window.innerHeight - targetElement.clientHeight) / 2;
+
+              // Realiza el desplazamiento suave
+              window.scrollTo({
+                  top: scrollTarget,
+                  behavior: 'smooth'
+              });
+          }
+      });
+  });
+});
